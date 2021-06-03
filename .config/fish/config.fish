d13a245f0f03c4d3d7af3b6d2908eea7870489af
Daniel Lin <dclin02@gmail.com>
2021-06-03 19:41:41 -0300

    switch from yakuake to alacritty with tmux; change exa ls alias; fix i3status-rust new config format;

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,7 +3,7 @@ load_nvm
 thefuck --alias | source
 fish_vi_key_bindings
 alias man="batman"
-alias ls="exa"
+alias ls="exa -a --color=always --group-directories-first --icons"
 alias lg="lazygit"
 alias tray="/home/berin/tray"
 # alias display /home/berin/display_setup.sh
