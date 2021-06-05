4321039e0bb653d78ae23f82a3f38cb5a13e6c4d
Daniel Lin <dclin02@gmail.com>
2021-06-05 11:24:30 -0300

    fix display_setup was symbolic link

--- display_setup.sh
+++ display_setup.sh
@@ -1,1 +1,6 @@
-/etc/lightdm/display_setup.sh
+#!/bin/bash
+xrandr --setprovideroutputsource modesetting NVIDIA-0
+# xrandr --auto
+xrandr --output eDP-1-1 --mode 1920x1080 --rate 144
+xrandr --output DP-0 --primary --mode 1920x1080 --rate 144 --left-of eDP-1-1
+xrandr --output HDMI-0 --mode 1920x1080 --rate 60 --left-of DP-0
