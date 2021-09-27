2d57ca416994a605ebb351456bf99d5b69b3edc7
Daniel Lin <dclin02@gmail.com>
2021-09-27 14:57:15 -0300

    update display setup script for new home office layout

--- display_setup.sh
+++ display_setup.sh
@@ -2,5 +2,5 @@
 xrandr --setprovideroutputsource modesetting NVIDIA-0
 # xrandr --auto
 xrandr --output eDP-1-1 --mode 1920x1080 --rate 144
-xrandr --output DP-0 --primary --mode 1920x1080 --rate 144 --left-of eDP-1-1
-xrandr --output HDMI-0 --mode 1920x1080 --rate 60 --left-of DP-0
+xrandr --output DP-0 --primary --mode 1920x1080 --rate 144 --right-of eDP-1-1
+xrandr --output HDMI-0 --mode 1360x768 --rate 60 --right-of DP-0
