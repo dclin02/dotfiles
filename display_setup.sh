04c9aeddee8a70244b21177e1ba554ef4c007077
Daniel Lin <dclin02@gmail.com>
2021-10-08 08:10:14 -0300

    fix display_setup script - add sleep between commands for panning bug with the HDMI 720p TV

--- display_setup.sh
+++ display_setup.sh
@@ -2,5 +2,7 @@
 xrandr --setprovideroutputsource modesetting NVIDIA-0
 # xrandr --auto
 xrandr --output eDP-1-1 --mode 1920x1080 --rate 144
+sleep 1
 xrandr --output DP-0 --primary --mode 1920x1080 --rate 144 --right-of eDP-1-1
+sleep 1
 xrandr --output HDMI-0 --mode 1360x768 --rate 60 --right-of DP-0
