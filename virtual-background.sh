6b117672a9d1f2f98813bad71f6e5ef57484f204
Daniel Lin <dclin02@gmail.com>
2022-01-04 19:28:08 -0300

    virtual background script used by vb fish alias

---
+++ virtual-background.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo modprobe v4l2loopback devices=1  exclusive_caps=1 video_nr=2 card_label="Virtual Background"
+
+cd /home/berin/Linux-Fake-Background-Webcam/
+bash -c "python fake.py --no-foreground"
