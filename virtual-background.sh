4cda4bb45943ff9a6cf448edfc4c4c9d80dac5a8
Daniel Lin <dclin02@gmail.com>
2022-01-04 21:13:02 -0300

    update virtual background script params

--- virtual-background.sh
+++ virtual-background.sh
@@ -2,4 +2,4 @@
 sudo modprobe v4l2loopback devices=1  exclusive_caps=1 video_nr=2 card_label="Virtual Background"
 
 cd /home/berin/Linux-Fake-Background-Webcam/
-bash -c "python fake.py --no-foreground"
+bash -c "python fake.py --no-foreground -C MJPG --fps 30 -W 1920 -H 1080"
