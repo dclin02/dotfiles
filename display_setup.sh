ada72f0a46e16c5b6e17ca103ead531c1e76cd16
Daniel Lin <dclin02@gmail.com>
2022-02-17 09:54:41 -0300

    display_setup to new room setup (main monitor left) with tray

--- display_setup.sh
+++ display_setup.sh
@@ -4,7 +4,7 @@ xrandr --setprovideroutputsource modesetting NVIDIA-0
 xrandr --output eDP-1-1 --mode 1920x1080 --rate 144
 # sleep 1
 # xrandr --output DP-0 --primary --mode 1920x1080 --rate 144 --left-of eDP-1-1
-xrandr --output DP-0 --primary --mode 1920x1080 --rate 144 --right-of eDP-1-1
+xrandr --output DP-0 --primary --mode 1920x1080 --rate 144 --left-of eDP-1-1
 # sleep 1
 xrandr --output HDMI-0 --mode 1920x1080 --rate 60 --same-as DP-0
 # sleep 1
