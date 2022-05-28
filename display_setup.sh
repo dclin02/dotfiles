ffb6415dc7a1b14a2ce1b8eb7e4e64308e6bb7f3
Daniel Lin <dclin02@gmail.com>
2022-05-28 17:49:31 -0300

    i3drop python script cause i3dropdown broke again; and display vertical monitor new settings

--- display_setup.sh
+++ display_setup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 xrandr --setprovideroutputsource modesetting NVIDIA-0
 # xrandr --auto
-xrandr --output eDP-1-1 --mode 1920x1080 --rate 144
+# xrandr --output eDP-1-1 --mode 1920x1080 --rate 144
 # sleep 1
 # xrandr --output DP-0 --primary --mode 1920x1080 --rate 144 --left-of eDP-1-1
-xrandr --output DP-0 --primary --mode 1920x1080 --rate 144 --left-of eDP-1-1
+# xrandr --output DP-0 --primary --mode 1920x1080 --rate 144 --left-of eDP-1-1
 # sleep 1
-xrandr --output HDMI-0 --mode 1920x1080 --rate 60 --same-as DP-0
+# xrandr --output HDMI-0 --mode 1920x1080 --rate 60 --same-as DP-0
 # sleep 1
 # feh --bg-scale /usr/share/backgrounds/i3_default_background.jpg
+xrandr --output DP-0 --mode 1920x1080 --rate 144 --primary --pos 0x420 --output HDMI-0 --mode 1920x1080 --rotate left --pos 1920x0 --output eDP-1-1 --off
+sleep 1
+feh --bg-scale /usr/share/backgrounds/i3_default_background.jpg
+# xrandr --output DP-0 --mode 1920x1080 --rate 144 --primary --output HDMI-0 --mode 1920x1080 --right-of DP-0 --ouput eDP-1-1 --off
