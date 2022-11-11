09dc64b33365f232d5639761e9d10bac01d7e974
Daniel Lin <dclin02@gmail.com>
2022-11-10 23:56:46 -0300

    fix display_setup for unplugged init

--- display_setup.sh
+++ display_setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 xrandr --setprovideroutputsource modesetting NVIDIA-0
-# xrandr --auto
+xrandr --auto
 # xrandr --output eDP-1-1 --mode 1920x1080 --rate 144
-# sleep 1
+sleep 1
 # xrandr --output DP-0 --primary --mode 1920x1080 --rate 144 --left-of eDP-1-1
 # xrandr --output DP-0 --primary --mode 1920x1080 --rate 144 --left-of eDP-1-1
 # sleep 1
