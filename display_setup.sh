470b4e687611397efb3f4f0a77a92a6d33c110f1
Daniel Lin <dclin02@gmail.com>
2023-02-07 22:57:26 -0300

    i3 and diplay_setup to new office setup: vertical+main+note screens, with new 0 and 11 workspaces with rebinds; new i3-resurrect save files

--- display_setup.sh
+++ display_setup.sh
@@ -9,7 +9,8 @@ sleep 1
 # xrandr --output HDMI-0 --mode 1920x1080 --rate 60 --same-as DP-0
 # sleep 1
 # feh --bg-scale /usr/share/backgrounds/i3_default_background.jpg
-xrandr --output DP-0 --mode 1920x1080 --rate 144 --primary --pos 0x420 --output HDMI-0 --mode 1920x1080 --rotate left --pos 1920x0 --output eDP-1-1 --mode 1920x1080 --same-as DP-0
+# xrandr --output DP-0 --mode 1920x1080 --rate 144 --primary --pos 0x420 --output HDMI-0 --mode 1920x1080 --rotate left --pos 1920x0 --output eDP-1-1 --mode 1920x1080 --same-as DP-0
+xrandr --output DP-0 --mode 1920x1080 --rate 144 --primary --pos 1080x420 --output HDMI-0 --mode 1920x1080 --rotate left --pos 0x0 --output eDP-1-1 --mode 1920x1080 --pos 3000x420
 sleep 1
 feh --bg-scale /usr/share/backgrounds/i3_default_background.jpg
 # xrandr --output DP-0 --mode 1920x1080 --rate 144 --primary --output HDMI-0 --mode 1920x1080 --right-of DP-0 --ouput eDP-1-1 --off
