589ae8fba2c598f81f8b5c024e6e63d29be805e6
Daniel Lin <dclin02@gmail.com>
2023-08-01 19:40:31 -0300

    fish: add aliases for tmux sessions for notes, hdmi, edp

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,9 @@ alias cat="bat"
 alias ls="exa -a --color=always --group-directories-first --icons"
 alias lg="lazygit"
 alias tray="/home/berin/tray"
+alias notes="tmux attach-session -t notes || tmux new -s notes \"z isaac & lvim notes.txt\""
+alias hdmi="tmux attach-session -t hdmi || tmux new -s hdmi"
+alias edp="tmux attach-session -t edp || tmux new -s edp"
 # alias display /home/berin/display_setup.sh
 starship init fish | source
 
@@ -20,6 +23,10 @@ alias kubectl='/home/berin/.asdf/installs/gcloud/412.0.0/bin/kubectl'
 direnv hook fish | source
 set -gx EDITOR lvim
 set -gx BROWSER /usr/bin/firefox
+
+# VLC fix should be temporary
+set -gx LIBVA_DRIVER_NAME vdpau
+set -gx VDPAU_DRIVER nvidia
 
 fish_add_path (go env GOPATH)/bin
 
