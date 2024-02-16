368259fb9d354851dce6b6bdd7a3a5daa01126cb
Daniel Lin <dclin02@gmail.com>
2024-02-16 16:18:43 -0300

    display_setup.sh: backup commented lines of alternate configs (salto etc)

--- display_setup.sh
+++ display_setup.sh
@@ -10,7 +10,14 @@ sleep 1
 # sleep 1
 # feh --bg-scale /usr/share/backgrounds/i3_default_background.jpg
 # xrandr --output DP-0 --mode 1920x1080 --rate 144 --primary --pos 0x420 --output HDMI-0 --mode 1920x1080 --rotate left --pos 1920x0 --output eDP-1-1 --mode 1920x1080 --same-as DP-0
+
+# current
 xrandr --output DP-0 --mode 1920x1080 --rate 144 --primary --pos 1080x420 --output HDMI-0 --mode 1920x1080 --rotate left --pos 0x0 --output eDP-1-1 --mode 1920x1080 --pos 3000x420
+
+# xrandr --output DP-0 --mode 1920x1080 --rate 144 --primary --output HDMI-0 --mode 1920x1080 --right-of DP-0 --ouput eDP-1-1 --off
+
+# Salto temp
+# xrandr --output HDMI-0 --rate 74.97 --primary --mode 1920x1080 --output eDP-1-1 --mode 1920x1080 --rate 144 --left-of HDMI-0
+
 sleep 1
 feh --bg-scale /usr/share/backgrounds/i3_default_background.jpg
-# xrandr --output DP-0 --mode 1920x1080 --rate 144 --primary --output HDMI-0 --mode 1920x1080 --right-of DP-0 --ouput eDP-1-1 --off
