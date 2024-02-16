434fb897d1384950a6277bdda27d8a1ff9b56de4
Daniel Lin <dclin02@gmail.com>
2024-02-16 16:12:42 -0300

    fish add weather, work, kb aliases and fnm config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,6 +9,8 @@ alias tray="/home/berin/tray"
 alias notes="tmux attach-session -t notes || tmux new -s notes \"z isaac & lvim notes.txt\""
 alias hdmi="tmux attach-session -t hdmi || tmux new -s hdmi"
 alias edp="tmux attach-session -t edp || tmux new -s edp"
+alias tempo="curl 'wttr.in/~USP?2QF'"
+alias tempo2="curl 'v2.wttr.in/~USP?2QF'"
 # alias display /home/berin/display_setup.sh
 starship init fish | source
 
@@ -19,6 +21,8 @@ alias vb='/home/berin/virtual-background.sh'
 alias panda='/home/berin/Gopanda2.AppImage'
 alias tf='terraform'
 alias kubectl='/home/berin/.asdf/installs/gcloud/412.0.0/bin/kubectl'
+alias work "/home/berin/work.sh"
+alias kb "/home/berin/keyboard-configure.sh"
 
 direnv hook fish | source
 set -gx EDITOR lvim
@@ -33,3 +37,4 @@ fish_add_path (go env GOPATH)/bin
 abbr -a ld lazydocker
 
 source /opt/asdf-vm/asdf.fish
+fnm env | source
